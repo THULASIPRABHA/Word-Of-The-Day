26a948e7ad0007f93ccf2d31554b7830fa1abafa
THULASIPRABHA <69714505+THULASIPRABHA@users.noreply.github.com>
2021-01-27 15:57:05 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+import requests
+import mwparserfromhell
+from datetime import date
+import re
+today = date.today()
+fdate = date.today().strftime('%Y/%B %d')
+title="Wiktionary:Word of the day/"+ fdate
+S = requests.Session()
+URL = "https://en.wiktionary.org/w/api.php"
+params={
+        'action': 'query',
+        'format': 'json',
+        'titles': title,
+        'prop': 'revisions',
+        'rvprop': 'content',
+        "formatversion":"2",
+   }
+R = S.get(url=URL, params=params)
+DATA = R.json()
+
+
+pages=DATA['query']['pages'][0]['revisions'][0]['content']
+
+wiki = re.compile(r'\[\[(?:[^|\]]*\|)?([^\]]+)\]\]')
+w=wiki.sub(r'\1', pages)
+print(w)
